2ef31dfea170767c4c2c22156cffc9cd3b8c58aa
Leung Yin <leungyinchiew@gmail.com>
2024-09-09 17:14:40 +0800

    Initial Commit

---
+++ TeamBet@CruiseProject/TeamBet_CruiseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamBet_CruiseProjectApp.swift
+//  TeamBet@CruiseProject
+//
+//  Created by Leung Yin on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct TeamBet_CruiseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
