358b53565678d3cd2c4623b93916fe4e1e8c08c2
Leung Yin <leungyinchiew@gmail.com>
2024-09-10 11:46:21 +0800

    Commit: Added AnalysisView and ReportDataView

---
+++ TeamBet@CruiseProject/AnalysisView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalysisView.swift
+//  TeamBet@CruiseProject
+//
+//  Created by Leung Yin on 10/9/24.
+//
+
+import SwiftUI
+
+struct AnalysisView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalysisView()
+}
---
+++ TeamBet@CruiseProject/ReportDataView.swift
@@ -0,0 +1,18 @@
+//
+//  ReportDataView.swift
+//  TeamBet@CruiseProject
+//
+//  Created by Leung Yin on 10/9/24.
+//
+
+import SwiftUI
+
+struct ReportDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReportDataView()
+}
